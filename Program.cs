dd313586517df7fdf0a05584ba88130ad4c1836d
mohamed <mohamed@gmail.com>
2024-07-15 23:42:40 +0300

    overloading

--- Program.cs
+++ Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        public static int Sum2nums(int x,int y)
+        {
+            return x + y;
+        }
+        public static int Sum3nums(int x,int y,int z)
+        {
+            return x + y+z;
+        } public static int Sum4nums(int x,int y,int z ,int m)
+        {
+            return x + y+z+m;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sum2nums(1, 2);
         }
     }
 }
